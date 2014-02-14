dc57c3155b2ac312eb00e14670ae46936e6f32e9
soasme <soasme@gmail.com>
2014-02-14 10:25:37 +0800

    Anyway, ground moves.

--- scripts/app/scenes/GameScene.lua
+++ scripts/app/scenes/GameScene.lua
@@ -16,6 +16,18 @@ end
 function GameScene:loadGround()
     self.ground = display.newSprite(GROUND_FILENAME, display.cx, display.bottom)
     self:addChild(self.ground)
+    self:moveGround()
+end
+
+function GameScene:moveGround()
+    self.ground:runAction(
+        CCRepeatForever:create(
+            transition.sequence({
+                CCMoveTo:create(0.6, ccp(100, 0)),
+                CCMoveTo:create(0, ccp(400, 0)),
+            })
+        )
+    )
 end
 
 return GameScene
