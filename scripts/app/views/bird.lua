5fc2cf2be4cee8ffcdf6c56131d862b33476385b
soasme <soasme@gmail.com>
2014-02-14 22:58:46 +0800

    Basic fly action.

--- scripts/app/views/bird.lua
+++ scripts/app/views/bird.lua
@@ -4,9 +4,9 @@ local Bird = class('Bird', function()
 end)
 
 function Bird:flap()
-    local frames = display.newFrames("bird%d.png", 1, 3, true)
-    local animation = display.newAnimation(frames, 0.5 / 3)
-    self:playAnimationForever(animation)
+    self.frames = display.newFrames("bird%d.png", 1, 3, true)
+    self.animation = display.newAnimation(self.frames, 0.5 / 3)
+    self:playAnimationForever(self.animation)
 end
 
 function Bird:flyUpAndDown()
@@ -19,6 +19,33 @@ function Bird:flyUpAndDown()
                 CCMoveBy:create(0.2, ccp(0, 0))
             })
         )
+    )
+end
+
+function Bird:fly()
+    local riseHeight = 60
+    local x = self:getPositionX()
+    local y = self:getPositionY()
+    local time = y / 600
+
+    local raise = CCSpawn:createWithTwoActions(
+        CCMoveTo:create(0.2, ccp(x, y + riseHeight)),
+        CCRotateTo:create(0, -30)
+    )
+    local fallen = CCSpawn:createWithTwoActions(
+        CCMoveTo:create(time, ccp(x, 50)),
+        transition.sequence({
+            CCDelayTime:create(time / 6),
+            CCRotateTo:create(0, 30)
+        })
+    )
+    self:stopAllActions()
+    self:runAction(
+        transition.sequence({
+            raise,
+            CCDelayTime:create(0.1),
+            fallen
+        })
     )
 end
 
