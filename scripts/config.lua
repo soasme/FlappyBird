25ccedd84c9ba0014bc3261f8b3e3dec8a832dad
soasme <soasme@gmail.com>
2014-02-14 08:55:49 +0800

    Define bg, ground file.

--- scripts/config.lua
+++ scripts/config.lua
@@ -2,3 +2,6 @@ TEXTURES_IMAGE_FILENAME = 'flappy_packer.png'
 
 TEXTURES_DATA_FILENAME = 'flappy_packer.plist'
 FLAPPY_FRAME_DATA_FILENAME = 'flappy_frame.plist'
+
+BACKGROUND_FILENAME = 'bg.png'
+GROUND_FILENAME = 'ground.png'
