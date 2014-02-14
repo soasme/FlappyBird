916501af1e9e71d48acf51355b0ef6ba8e20401a
soasme <soasme@gmail.com>
2014-02-14 16:44:44 +0800

    Render background in game scene.

--- scripts/app/scenes/GameScene.lua
+++ scripts/app/scenes/GameScene.lua
@@ -1,8 +1,16 @@
+local Bird = import('..views.bird')
+local Background = import('..views.background')
 local GameScene = class('GameScene', function()
     return display.newScene('GameScene')
 end)
 
 function GameScene:ctor()
+    self:loadBackground()
+end
+
+function GameScene:loadBackground()
+    local background = Background.new()
+    self:addChild(background)
 end
 
 return GameScene
