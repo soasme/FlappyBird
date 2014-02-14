b75d909b479159b63e1b4be5edbc2cab2a1926e4
soasme <soasme@gmail.com>
2014-02-14 16:38:25 +0800

    Extract Background as view.

---
+++ scripts/app/views/background.lua
@@ -0,0 +1,32 @@
+local Background = class('Background', function()
+    return display.newLayer()
+end)
+
+function Background:ctor()
+    self:loadBackground()
+    self:loadGround()
+end
+
+function Background:loadBackground()
+    self.bg = display.newSprite(BACKGROUND_FILENAME, display.cx, display.cy)
+    self:addChild(self.bg)
+end
+
+function Background:loadGround()
+    self.ground = display.newSprite(GROUND_FILENAME, display.cx, display.bottom)
+    self:addChild(self.ground)
+    self:moveGround()
+end
+
+function Background:moveGround()
+    self.ground:runAction(
+        CCRepeatForever:create(
+            transition.sequence({
+                CCMoveTo:create(0.5, ccp(display.cx - 60, display.bottom)),
+                CCMoveTo:create(0, ccp(display.cx, display.bottom))
+            })
+        )
+    )
+end
+
+return Background
