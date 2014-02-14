5a5620597762e9577acc921d6ac758477de9c55a
soasme <soasme@gmail.com>
2014-02-14 09:42:11 +0800

    Load ground

--- scripts/app/scenes/GameScene.lua
+++ scripts/app/scenes/GameScene.lua
@@ -4,8 +4,18 @@ local GameScene = class('GameScene', function()
 end)
 
 function GameScene:ctor()
+    self:loadBackground()
+    self:loadGround()
+end
+
+function GameScene:loadBackground()
     self.bg = display.newSprite(BACKGROUND_FILENAME, display.cx, display.cy)
     self:addChild(self.bg)
+end
+
+function GameScene:loadGround()
+    self.ground = display.newSprite(GROUND_FILENAME, display.cx, display.bottom)
+    self:addChild(self.ground)
 end
 
 return GameScene
