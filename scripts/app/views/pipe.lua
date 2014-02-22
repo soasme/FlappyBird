bc3c3db18bb178c1de6b75f05d2267148972e1a1
soasme <soasme@gmail.com>
2014-02-22 23:11:22 +0800

    Create pipe sprite(up + down)

--- scripts/app/views/pipe.lua
+++ scripts/app/views/pipe.lua
@@ -10,20 +10,34 @@ function Pipe:ctor(world)
     local offset = 95
     local groundHeight = 140
     local random = math.random(300, display.top - 200)
+    local width = display.width / 8
 
     local downSectionHeight = random - offset - groundHeight
-    self.downSection = self.world:createBoxBody(1, display.width / 8, downSectionHeight)
+    self.downSection = self.world:createBoxBody(1, width, downSectionHeight)
     self.downSection:setPosition(display.width, groundHeight + 0.5 * downSectionHeight)
     self.downSection:setVelocity(VELOCITY, 0)
     self.downSection:setCollisionType(CollisionType.pipe)
 
+    self.downSectionSprite = display.newSprite('#holdback1.png')
+    self.downSectionSprite:setTextureRect(CCRectMake(
+        1, 479, 148, downSectionHeight
+    ))
+    self.downSectionSprite:setScaleX(width / 148)
+    self.downSection:bind(self.downSectionSprite)
+
     local upSectionHeight = display.top - random - offset
-    self.upSection = self.world:createBoxBody(1, display.width / 8, upSectionHeight)
+    self.upSection = self.world:createBoxBody(1, width, upSectionHeight)
     self.upSection:setPosition(display.width, display.top - 0.5 * upSectionHeight)
     self.upSection:applyForce(0, -GRAVITY, 0, 0)
     self.upSection:setVelocity(VELOCITY, 0)
     self.upSection:setCollisionType(CollisionType.pipe)
-end
 
+    self.upSectionSprite = display.newSprite('#holdback2.png')
+    self.upSectionSprite:setTextureRect(CCRectMake(
+        150, 459, 148, 830 + (830 - upSectionHeight)
+    ))
+    self.upSectionSprite:setScaleX(width / 148)
+    self.upSection:bind(self.upSectionSprite)
+end
 
 return Pipe
