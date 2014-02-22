f38572755865434c2be0eab3bead01a7030cfeef
soasme <soasme@gmail.com>
2014-02-22 21:55:03 +0800

    Set Pipe body position.

--- scripts/app/views/pipe.lua
+++ scripts/app/views/pipe.lua
@@ -2,18 +2,26 @@ local Pipe = class('Pipe', function()
     return display.newLayer()
 end)
 
+VELOCITY = -100
+
 function Pipe:ctor(world)
     self.world = world
     local height = 200
-    self.downSection = self.world:createBoxBody(1, display.width / 8, height)
-    self.downSection:setPosition(display.width, height)
-    self.downSection:setVelocity(-100, 0)
+    local offset = 100
+    local groundHeight = 150
+    local random = math.random(300, display.top - 200)
+
+    local downSectionHeight = random - offset - groundHeight
+    self.downSection = self.world:createBoxBody(1, display.width / 8, downSectionHeight)
+    self.downSection:setPosition(display.width, groundHeight + 0.5 * downSectionHeight)
+    self.downSection:setVelocity(VELOCITY, 0)
     self.downSection:setCollisionType(CollisionType.pipe)
 
-    self.upSection = self.world:createBoxBody(1, display.width / 8, height)
-    self.upSection:setPosition(display.width, 500)
+    local upSectionHeight = display.top - random - offset
+    self.upSection = self.world:createBoxBody(1, display.width / 8, upSectionHeight)
+    self.upSection:setPosition(display.width, display.top - 0.5 * upSectionHeight)
     self.upSection:applyForce(0, 300, 0, 0)
-    self.upSection:setVelocity(-100, 0)
+    self.upSection:setVelocity(VELOCITY, 0)
     self.upSection:setCollisionType(CollisionType.pipe)
 end
 
