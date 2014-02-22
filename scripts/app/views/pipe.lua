5e31e4abeb86f307eec1a85378a1f8b02ff0d1c6
soasme <soasme@gmail.com>
2014-02-22 23:40:19 +0800

    It should set texture rect before binding.

--- scripts/app/views/pipe.lua
+++ scripts/app/views/pipe.lua
@@ -6,38 +6,40 @@ VELOCITY = -100
 
 function Pipe:ctor(world)
     self.world = world
+    self.downSectionSprite = CCSprite:createWithSpriteFrameName('holdback1.png')
+    self.upSectionSprite = CCSprite:createWithSpriteFrameName('holdback2.png')
     local height = 200
     local offset = 95
     local groundHeight = 140
     local random = math.random(300, display.top - 200)
-    local width = display.width / 8
+    local width = display.width / 6
 
     local downSectionHeight = random - offset - groundHeight
     self.downSection = self.world:createBoxBody(1, width, downSectionHeight)
-    self.downSection:setPosition(display.width, groundHeight + 0.5 * downSectionHeight)
-    self.downSection:setVelocity(VELOCITY, 0)
-    self.downSection:setCollisionType(CollisionType.pipe)
-
-    self.downSectionSprite = display.newSprite('#holdback1.png')
     self.downSectionSprite:setTextureRect(CCRectMake(
         1, 479, 148, downSectionHeight
     ))
     self.downSectionSprite:setScaleX(width / 148)
     self.downSection:bind(self.downSectionSprite)
+    self.downSection:setPosition(display.width, groundHeight + 0.5 * downSectionHeight)
+    self.downSection:setVelocity(VELOCITY, 0)
+    self.downSection:setCollisionType(CollisionType.pipe)
+
 
     local upSectionHeight = display.top - random - offset
     self.upSection = self.world:createBoxBody(1, width, upSectionHeight)
-    self.upSection:setPosition(display.width, display.top - 0.5 * upSectionHeight)
-    self.upSection:applyForce(0, -GRAVITY, 0, 0)
-    self.upSection:setVelocity(VELOCITY, 0)
-    self.upSection:setCollisionType(CollisionType.pipe)
-
-    self.upSectionSprite = display.newSprite('#holdback2.png')
     self.upSectionSprite:setTextureRect(CCRectMake(
         150, 459, 148, 830 + (830 - upSectionHeight)
     ))
     self.upSectionSprite:setScaleX(width / 148)
     self.upSection:bind(self.upSectionSprite)
+    self.upSection:setPosition(display.width, display.top - 0.5 * upSectionHeight)
+    self.upSection:applyForce(0, -GRAVITY, 0, 0)
+    self.upSection:setVelocity(VELOCITY, 0)
+    self.upSection:setCollisionType(CollisionType.pipe)
+
 end
+
+-- Note: It should set texture rect before binding.
 
 return Pipe
