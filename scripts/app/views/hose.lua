1f63838c2f0208097baf3fd6b2d8e6981beb1e04
soasme <soasme@gmail.com>
2014-02-22 16:07:34 +0800

    Create Hose.

--- scripts/app/views/hose.lua
+++ scripts/app/views/hose.lua
@@ -6,21 +6,38 @@ local Hose = class('Hose', function(beginOffset)
     return node
 end)
 
-function Hose:ctor()
+function Hose:ctor(world)
+    self.world = world
     self.center = display.height / 2
     self.up = CCSprite:createWithSpriteFrameName('holdback1.png')
     self.down = CCSprite:createWithSpriteFrameName('holdback2.png')
     local ratio = display.width / (6.0 * self.up:getContentSize().width )
-    local upHeight = self.up:getContentSize().height
     self.up:setScaleX(ratio)
     self.up:setScaleY(ratio)
     self.down:setScaleX(ratio)
     self.down:setScaleY(ratio)
 
-    down_height = self.center + math.random(200, 600)
-    up_height = down_height - 620
-    self.down:setPosition(ccp(display.right + self.beginOffset, down_height))
-    self.up:setPosition(ccp(display.right + self.beginOffset, up_height))
+    local random = math.random(200, 600)
+    local width = display.width / 6
+    local offset = 100
+    local groundHeight = 140
+    local upBoxHeight = random - offset - groundHeight
+    local downBoxHeight = display.height - random - offset
+    self.upBox = world:createBoxBody(1, width, upBoxHeight)
+    self.upBox:setCollisionType(CollisionType.hose)
+    self.upBox:setPosition(ccp(display.width, groundHeight + 0.5 * upBoxHeight))
+    self.upBox:applyForce(-20, 300, 0, 0)
+    self.downBox = world:createBoxBody(1, width, downBoxHeight)
+    self.downBox:setCollisionType(CollisionType.hose)
+    self.downBox:setPosition(ccp(display.width, downBoxHeight * 0.5 + offset + random))
+    self.downBox:applyForce(-20, 300, 0, 0)
+    self.up:setTextureRect(CCRectMake(1, 459, 148, upBoxHeight / ratio + 40))
+    self.down:setTextureRect(CCRectMake(150, 459 + (790 - downBoxHeight) * ratio, 148, downBoxHeight / ratio))
+    self.upBox:bind(self.up)
+    self.downBox:bind(self.down)
+end
+
+function Hose:resetPosition()
 end
 
 
